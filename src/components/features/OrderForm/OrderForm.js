c241466234a90860924a89596ad39cc6e39aaae9
Igor Jankowski <jankowskiigor5@gmail.comgit congig --global -lgit config --global user.email jankowskiigor5@gmail.com>
2020-12-21 23:49:34 +0100

    home

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,20 +2,32 @@ import React from 'react';
 import OrderSummary from '../OrderSummary/OrderSummary.js';
 import {Row, Col, Grid} from 'react-flexbox-grid';
 import PropTypes from 'prop-types';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption.js';
 
-const OrderForm = ({options, tripCost}) => (
-  <Grid>
-    <Row>
-      <Col xs={12}>
-        <OrderSummary tripCost={tripCost} tripOptions={options}/>
-      </Col>
-    </Row>
-  </Grid>
-);
+const OrderForm = ({options, tripCost, setOrderOption}) => {
+  return(
+    <Grid>
+      <Row>
+        {pricing.map(option => (
+          <Col md={4} key={option.id}>
+            <OrderOption {...option}
+              currentValue={options[option.id]}
+              setOrderOption={setOrderOption}/>
+          </Col>
+        ))}
+        <Col xs={12}>
+          <OrderSummary tripCost={tripCost} tripOptions={options}/>
+        </Col>
+      </Row>
+    </Grid>
+  );
+};
 
 OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
+  setOrderOption: PropTypes.func,
 };
 
 
--- src/components/features/OrderForm/OrderFormContainer.js
+++ src/components/features/OrderForm/OrderFormContainer.js
@@ -1,12 +1,16 @@
 import {connect} from 'react-redux';
 import {getOrderOptions} from '../../../redux/orderRedux';
 import OrderForm from './OrderForm.js';
-
+import setOrderOption from '../../../redux/orderRedux';
 
 const mapStateToProps = state => ({
   options: getOrderOptions(state),
+  setOrderOption: setOrderOption(state),
 });
 
+const mapDispatchToProps = dispatch => ({
+  setOrderOption: option => dispatch(setOrderOption(option)),
+});
 
-export default connect(mapStateToProps)(OrderForm);
+export default connect(mapStateToProps, mapDispatchToProps)(OrderForm);
 
